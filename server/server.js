4638adbfa83c63ad11a0b6bd4af62a6ed61718dd
Ernest Belicic <ernest.belicic@gmail.com>
2015-06-30 18:43:23 +0200

    server ki nadzoruje motorcek

---
+++ server/server.js
@@ -0,0 +1,24 @@
+var gpio = require("rpi-gpio");
+var connect = require("connect");
+
+gpio.setup(8, gpio.DIR_OUT);
+app = connect();
+
+app.use("/on", function(req, res, next){
+    gpio.write(8, true);
+    res.end();
+});
+
+app.use("/off", function(req, res, next){
+    gpio.write(8, false);
+    res.end();
+});
+
+app.listen(80);
+console.log("server online");
+
+process.on("SIGINT", function(){
+    console.log("exiting");
+    gpio.destroy();
+    process.exit();
+});
