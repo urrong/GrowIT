ffc8ffb7939271aa94df3fe654ffec41b53011ec
Ernest Belicic <ernest.belicic@gmail.com>
2015-07-29 00:37:59 +0000

    v4l2

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var gpio = require("rpi-gpio");
 var connect = require("connect");
+var v4l2camera = require("v4l2camera");
 
 gpio.setup(8, gpio.DIR_OUT);
 app = connect();
@@ -14,11 +15,19 @@ app.use("/off", function(req, res, next){
     res.end();
 });
 
-app.listen(80);
+//app.listen(80);
 console.log("server online");
 
 process.on("SIGINT", function(){
     console.log("\nexiting");
     gpio.destroy();
     setTimeout(process.exit, 100);
+});
+
+var cam = new v4l2camera.Camera("/dev/video0");
+cam.start();
+cam.capture(function(success){
+    var rgb = cam.toRGB();
+    require("fs").writeFileSync("result.raw", Buffer(rgb));
+    cam.stop();
 });
