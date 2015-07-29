3111ccca9ba104b6c77c56efc3618e22572e174e
Ernest Belicic <ernest.belicic@gmail.com>
2015-07-29 18:46:18 +0000

    slow streaming implemented in index.html

--- server/server.js
+++ server/server.js
@@ -1,21 +1,32 @@
 var gpio = require("rpi-gpio");
 var connect = require("connect");
+var serveStatic = require("serve-static");
 var v4l2camera = require("v4l2camera");
+var JPEG = require("jpeg").Jpeg;
 
 gpio.setup(8, gpio.DIR_OUT);
 app = connect();
 
-app.use("/on", function(req, res, next){
+app.use("/won", function(req, res, next){
     gpio.write(8, true);
-    res.end();
+    res.end("True");
 });
 
-app.use("/off", function(req, res, next){
+app.use("/woff", function(req, res, next){
     gpio.write(8, false);
-    res.end();
+    res.end("True");
 });
 
-//app.listen(80);
+var streamResponse = null;
+app.use("/stream.mjpg", function(req, res, next){
+    console.log("client connected");
+    res.writeHead(200, {"Content-type": "multipart/x-mixed-replace; boundary=--jpgboundary"});
+    streamResponse = res;
+});
+
+app.use(serveStatic(__dirname + "/public"));
+
+app.listen(8080);
 console.log("server online");
 
 process.on("SIGINT", function(){
@@ -25,9 +36,28 @@ process.on("SIGINT", function(){
 });
 
 var cam = new v4l2camera.Camera("/dev/video0");
+var format = cam.configGet();
+format.width = 320;
+format.height = 240;
+cam.configSet(format);
+console.log(cam.configGet());
+//console.log(cam.formats);
+
 cam.start();
-cam.capture(function(success){
-    var rgb = cam.toRGB();
-    require("fs").writeFileSync("result.raw", Buffer(rgb));
-    cam.stop();
+cam.capture(function loop(success) {
+    if(!success){
+        console.log("Capture failed");
+    }
+    else{
+        var jpeg = new JPEG(Buffer(cam.toRGB()), cam.width, cam.height);
+        var image = jpeg.encodeSync();
+        if(streamResponse){
+            streamResponse.write("--jpgboundary\n");
+            streamResponse.write("Content-type: image/jpeg\n");
+            streamResponse.write("Content-length: " + image.length + "\n\n");
+            streamResponse.write(image);
+        }
+    }
+    //require("fs").writeFileSync("image.jpg", image);
+    cam.capture(loop);
 });
